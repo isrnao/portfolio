5ba6768669ac97198c69f60e4ac1bc8034af0106
isrnao <jggpad@gmail.com>
2024-07-19 22:43:07 +0900

    gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,25 @@
-import React from 'react'; // 手動で追加
+// src/pages/_document.tsx
+
+import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Tag Manager */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S45MQ7M1EJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S45MQ7M1EJ');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
